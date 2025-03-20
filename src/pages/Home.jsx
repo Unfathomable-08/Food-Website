bd962181e35ba99c45bcb2470e0bd37c02f3a67a
Huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2025-03-20 23:16:06 +0500

    Add to cart Functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import Navbar from "../components/Navbar"
+import Product from "../components/Product"
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
+      <Product/>
     </div>
   )
 }
